fc18e040ca74bd5a76b1365f5e56f424aa1a34ed
Nadezhda Savchenko <nadya73@ytsaurus.tech>
2024-06-29 15:15:52 +0300

    Add tests

---
+++ yt/yt/scripts/run_unittests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -ex
+# Should be run inside build directory
+
+# unittester-containers requires porto to be installed.
+for unittester_binary in $(find . -name "unittester-*" -type f | grep -v "unittester-containers"); do
+    echo "Running ${unittester_binary}"
+    unittester_name="$(basename ${unittester_binary})"
+    ${unittester_binary} --gtest_output="xml:junit-${unittester_name}.xml"
+done
+
+for unittester_binary in $(find . -name "*-ut" -type f); do
+    echo "Running ${unittester_binary}"
+    ${unittester_binary}
+done
