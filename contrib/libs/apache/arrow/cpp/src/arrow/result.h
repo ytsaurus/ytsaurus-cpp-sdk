2be77e132052350f590e7428ae0ba5fd9bf0c604
Nadezhda Savchenko <nadya73@ytsaurus.tech>
2024-11-17 20:53:57 +0300

    Fix arrow

--- contrib/libs/apache/arrow/cpp/src/arrow/result.h
+++ contrib/libs/apache/arrow/cpp/src/arrow/result.h
@@ -386,7 +386,7 @@ class ARROW_MUST_USE_TYPE Result : public util::EqualityComparable<Result<T>> {
   /// Apply a function to the internally stored value to produce a new result or propagate
   /// the stored error.
   template <typename M>
-  typename EnsureResult<typename std::result_of<M && (T)>::type>::type Map(M&& m) && {
+  typename EnsureResult<decltype(std::declval<M&&>()(std::declval<T&&>()))>::type Map(M&& m) && {
     if (!ok()) {
       return status();
     }
@@ -396,7 +396,7 @@ class ARROW_MUST_USE_TYPE Result : public util::EqualityComparable<Result<T>> {
   /// Apply a function to the internally stored value to produce a new result or propagate
   /// the stored error.
   template <typename M>
-  typename EnsureResult<typename std::result_of<M && (const T&)>::type>::type Map(
+  typename EnsureResult<decltype(std::declval<M&&>()(std::declval<const T&>()))>::type Map(
       M&& m) const& {
     if (!ok()) {
       return status();
